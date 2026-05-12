4718c258eee676cfda9725d61481b195a828d847
Francis <francis13322@gmail.com>
2026-05-12 15:15:50 -0300

    N03 - Criando um servidor Web com NodeJs

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello!");
+const http = require("http")
+
+let server = http.createServer((red, res)=>{
+
+    console.log("URL", red.url)
+    console.log("METHOD", red.method)
+
+    res.end("Ok")
+
+})
+
+server.listen(3000, "127.0.0.1", ()=>{
+
+    console.log("Servidor rodando!")
+
+})
