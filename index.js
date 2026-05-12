a42d97e48a4e62a6a3623eeae2e4634684ee27f7
Francis <francis13322@gmail.com>
2026-05-12 17:32:01 -0300

    N07 - Separando rotas do arquivo principal

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
 const express = require("express")
-//const { json } = require("stream/consumers")
+let routesIndex = require("./routes/index")
+let routesUsers = require("./routes/users")
+
+
 let app = express()
-app.get("/", (red, res)=>{
 
-    res.statusCode = 200
-    res.setHeader("Content-Type", "text/html")
-    res.end("<h1>Olá</h1>")
-
-})
-
-app.get("/users", (req, res)=>{
-
-    res.statusCode = 200
-            res.setHeader("Content-Type", "application/json")
-            res.json({
-
-                users:[{
-                    name:"Hcode",
-                    email: "contato@hcode.com",
-                    id: 2
-                }]
-
-            })
-
-})
+app.use(routesIndex)
+app.use("/users", routesUsers)
 app.listen(3000, "127.0.0.1", ()=>{
 
     console.log("Servidor rodando!")
