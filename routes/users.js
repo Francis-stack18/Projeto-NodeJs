5810d1c5b7a1ae587c47d5b5aaaa6f37021ebfd1
Francis <francis13322@gmail.com>
2026-05-13 19:36:17 -0300

    N13 - Obtendo dados de um usuario

--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,7 @@ let db = new NeDB({
 });
 
 module.exports = (app) => {
-
-  let route = app.route("/users")
+  let route = app.route("/users");
 
   route.get((req, res) => {
     db.find({})
@@ -29,6 +28,17 @@ module.exports = (app) => {
 
   route.post((req, res) => {
     db.insert(req.body, (err, user) => {
+      if (err) {
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(user);
+      }
+    });
+  });
+
+  let routeId = app.route("/users/:id");
+  routeId.get((req, res) => {
+    db.findOne({ _id: req.params.id }).exec((err, user) => {
       if (err) {
         app.utils.error.send(err, req, res);
       } else {
