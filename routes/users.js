aa05c3616e1e545c4a0e353c759cad4316b69b63
Francis <francis13322@gmail.com>
2026-05-13 18:59:20 -0300

    N10 - Persistencia de dados com o NeDB

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,12 @@
+const util = require("util")
+util.isDate = util.types.isDate
+util.isRegExp = util.types.isRegExp
+let NeDB = require("nedb");
+let db = new NeDB({
+  filename: "users.db",
+  autoload: true,
+});
+
 module.exports = (app) => {
   app.get("/users", (req, res) => {
     res.statusCode = 200;
@@ -14,6 +23,15 @@ module.exports = (app) => {
   });
 
   app.post("/users", (req, res) => {
-    res.json(req.body);
+    db.insert(req.body, (err, user) => {
+      if (err) {
+        console.log(`Error: ${err}`);
+        res.status(400).json({
+          error: err
+        });
+      } else {
+        res.status(200).json(user);
+      }
+    });
   });
 };
